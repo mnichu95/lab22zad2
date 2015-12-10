adba29707d42e5a59ed8ae05788ab21d1cd975e4
mnichu95 <218252@student.pwr.edu.pl>
2015-12-10 23:43:09 +0100

    Update ReceiveLifo.java

--- zad2po/src/zad2po/ReceiveLifo.java
+++ zad2po/src/zad2po/ReceiveLifo.java
@@ -5,7 +5,7 @@ public class ReceiveLifo extends MyAbstract {
 	Queue queue = new Queue();
 
 	@Override
-	public void Receiving() {
+	public void receiving() {
 		for (int i = queue.list.size() - 1; i >= 0; i--) {
 			System.out.println(queue.list.get(i));
 		}
